8df28b256dff5c7ba57368654230f2603a3cb333
VVoron <vkunievskiy@list.ru>
2023-09-06 17:21:28 +0300

    [studentsDatabase] Added tests and fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 struct Student {
     std::string name;
@@ -49,7 +51,6 @@ void readDataFromCSVFile(std::vector<Student>& database) {
     std::cout << "Введите название файла: ";
     std::string filename;
     std::cin >> filename;
-    std::cout << std::endl;
 
 
     char delimiter = ',';
@@ -59,7 +60,17 @@ void readDataFromCSVFile(std::vector<Student>& database) {
 
     std::ifstream file(filename);
 
+    if (file.fail()) {
+        throw "File doesn't exists";
+        return;
+    }
+
+    int num_str = 0;
+
     while (std::getline(file, line)) {
+        num_str++;
+        bool correct_input = true;
+
         std::stringstream stream(line);
 
         std::string student_data[4];
@@ -67,6 +78,17 @@ void readDataFromCSVFile(std::vector<Student>& database) {
         std::getline(stream, student_data[1], delimiter);
         std::getline(stream, student_data[2], delimiter);
         std::getline(stream, student_data[3], delimiter);
+
+        for (auto str : student_data) {
+            if (str.empty()) {
+                std::cout << "Value can't be null in " + std::to_string(num_str) + " row\n";
+                correct_input = false;
+                break;
+            }
+        }
+
+        if (!correct_input)
+            continue;
 
         Student student(student_data);
         database.push_back(student);
@@ -87,7 +109,101 @@ void getDatabaseCSVFile(const std::vector<Student>& database) {
     }
 
     file.close();
+    std::cout << "Выгрузка завершена\n";
+}
 
+void testForAddingStudent() {
+    std::vector<Student> database;
+
+    try
+    {
+        addStudent(database);
+    }
+    catch (const std::exception&)
+    {
+        std::cout << "Test for adding failed\n";
+    }
+
+    std::cout << "Test for adding passed\n";
+}
+
+void testForOutputFile() {
+    std::vector<Student> database;
+    std::string test[4]{ "Test", "40", "Test", "100" };
+    database.push_back(Student(test));
+
+    try
+    {
+        getDatabaseCSVFile(database);
+        std::ifstream file("Выгрузка.csv");
+        if (file.fail())
+            throw "Incorrect work";
+    }
+    catch (const std::exception&)
+    {
+        std::cout << "Test for get db file failed\n";
+    }
+
+    std::cout << "Test for get db file passed\n";
+}
+
+void testForReadingFilesCorrect() {
+    std::vector<Student> database;
+
+    try
+    {
+        readDataFromCSVFile(database);
+        if (database.size() != 4)
+            throw "Incorrect work";
+    }
+    catch (const std::exception&)
+    {
+        std::cout << "Test for reading 1 failed\n";
+    }
+
+    std::cout << "Test for reading 1 passed\n";
+}
+
+void testForReadingFilesIncorrect() {
+    std::vector<Student> database;
+
+    try
+    {
+        readDataFromCSVFile(database);
+        if (database.size() != 4)
+            throw "Incorrect work";
+    }
+    catch (const std::exception&)
+    {
+        std::cout << "Test for reading 1 failed\n";
+    }
+
+    std::cout << "Test for reading 1 passed\n";
+}
+
+void testForInCorrectFileData() {
+    std::vector<Student> database;
+
+    try
+    {
+        readDataFromCSVFile(database);
+        if (database.size() != 3)
+            throw "Incorrect work";
+    }
+    catch (const std::exception&)
+    {
+        std::cout << "Test for reading 2 failed\n";
+    }
+
+    std::cout << "Test for reading 2 passed\n";
+}
+
+void testsForApp() {
+    testForAddingStudent();
+    testForOutputFile();
+    testForReadingFilesCorrect();
+    testForReadingFilesIncorrect();
+    testForInCorrectFileData();
 }
 
 int main() {
@@ -107,23 +223,23 @@ int main() {
         std::cin >> choice;
 
         switch (choice) {
-            case 1:
-                addStudent(database);
-                break;
-            case 2:
-                displayStudents(database);
-                break;
-            case 3:
-                readDataFromCSVFile(database);
-                break;
-            case 4:
-                getDatabaseCSVFile(database);
-                break;
-            case 0:
-                std::cout << "Выход из программы.\n";
-                break;
-            default:
-                std::cout << "Неверный выбор. Попробуйте снова.\n";
+        case 1:
+            addStudent(database);
+            break;
+        case 2:
+            displayStudents(database);
+            break;
+        case 3:
+            readDataFromCSVFile(database);
+            break;
+        case 4:
+            getDatabaseCSVFile(database);
+            break;
+        case 0:
+            std::cout << "Выход из программы.\n";
+            break;
+        default:
+            std::cout << "Неверный выбор. Попробуйте снова.\n";
         }
     } while (choice != 0);
 
