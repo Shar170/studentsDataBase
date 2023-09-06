13583919a0a3995530acafa958eb496f7a0968c7
Ne-MoBu-u-Ne-ToNu <seriy.sery2014@gmail.com>
2023-09-06 15:49:50 +0300

    implemented interface for average scoring

--- main.cpp
+++ main.cpp
@@ -56,6 +56,7 @@ int main() {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Вывести сумму всех средних баллов студентов\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -66,6 +67,9 @@ int main() {
                 break;
             case 2:
                 displayStudents(database);
+                break;
+            case 3:
+                std::cout << "Сумма всех средних баллов студентов: " << averageScoreSum(database) << std::endl;
                 break;
             case 0:
                 std::cout << "Выход из программы.\n";
