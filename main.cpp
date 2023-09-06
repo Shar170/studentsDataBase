3d3f548e41051c0b66280fe49d579027e049d116
Kulyomin <kulemin200261@gmail.com>
2023-09-06 17:18:34 +0300

    Commit №2 Adding test functions, but they dont working

--- main.cpp
+++ main.cpp
@@ -43,12 +43,92 @@ void displayStudents(const std::vector<Student>& database) {
 
 void averageAgeStudents(const std::vector<Student>& database) {
     double averageAge = NULL;
-    for (const Student& student : database) {
-        averageAge += student.age;
+    if (database == NULL) {
+        break;
     }
-    averageAge = averageAge / database.size();
+    else {
+        for (const Student& student : database) {
+            averageAge += student.age;
+        }
+        averageAge = averageAge / database.size();
 
-    std::cout << averageAge << std::endl;
+        std::cout << averageAge << std::endl;
+    }
+}
+
+// Тест на пустую базу данных студентов
+void testEmptyDatabase(const std::vector<Student>& database) {
+    std::cout << "Тест на пустую базу данных:\n";
+    averageAgeStudents(database);
+    // Ожидается, что средний возраст будет равен 0
+}
+
+// Тест на одного студента
+void testSingleStudent(const std::vector<Student>& database) {
+    Student student;
+    student.name = "Иван";
+    student.age = 20;
+    student.major = "Инженерия";
+    student.gpa = 3.5;
+    database.push_back(student);
+
+    std::cout << "Тест на одного студента:\n";
+    averageAgeStudents(database);
+    // Ожидается, что средний возраст будет равен 20
+}
+
+// Тест на нескольких студентов с разными возрастами
+void testMultipleStudents(const std::vector<Student>& database) {
+    Student student1, student2, student3;
+    student1.name = "Анна";
+    student1.age = 21;
+    student1.major = "Информатика";
+    student1.gpa = 3.7;
+
+    student2.name = "Петр";
+    student2.age = 19;
+    student2.major = "Физика";
+    student2.gpa = 3.2;
+
+    student3.name = "Елена";
+    student3.age = 22;
+    student3.major = "Химия";
+    student3.gpa = 3.9;
+
+    database.push_back(student1);
+    database.push_back(student2);
+    database.push_back(student3);
+
+    std::cout << "Тест на нескольких студентов с разными возрастами:\n";
+    averageAgeStudents(database);
+    // Ожидается, что средний возраст будет правильно вычислен
+}
+
+// Тест на округление среднего возраста
+void testRoundedAverageAge(const std::vector<Student>& database) {
+    Student student1, student2, student3;
+    student1.name = "Анна";
+    student1.age = 21;
+    student1.major = "Информатика";
+    student1.gpa = 3.7;
+
+    student2.name = "Петр";
+    student2.age = 19;
+    student2.major = "Физика";
+    student2.gpa = 3.2;
+
+    student3.name = "Елена";
+    student3.age = 22;
+    student3.major = "Химия";
+    student3.gpa = 3.9;
+
+    database.push_back(student1);
+    database.push_back(student2);
+    database.push_back(student3);
+
+    std::cout << "Тест на округление среднего возраста:\n";
+    averageAgeStudents(database);
+    // Ожидается, что средний возраст будет округлен до ближайшего целого числа
 }
 
 int main() {
@@ -82,6 +162,12 @@ int main() {
             std::cout << "Неверный выбор. Попробуйте снова.\n";
         }
     } while (choice != 0);
+
+    // Вызываем функции-тесты
+    testEmptyDatabase(database);
+    testSingleStudent(database);
+    testMultipleStudents(database);
+    testRoundedAverageAge(database);
 
     return 0;
 }
