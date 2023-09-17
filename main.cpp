e4c468c60e4d80a2a9dc0981bdab75cfb04e9000
Katyana01 <100161894+Katyana01@users.noreply.github.com>
2023-09-17 17:06:11 +0300

    Add five Tests

--- main.cpp
+++ main.cpp
@@ -56,6 +56,184 @@ bool compareByGpa(const Student& a, const Student& b) {
     return a.gpa > b.gpa;
 }
 
+class Tests{
+    void Test1(){
+        // Arrange
+        std::vector<Student> database;
+        Student student;
+
+        student.name = "Katya";
+        student.age = 20;
+        student.major = "ICT";
+        student.gpa = 4.9;
+        database.push_back(student);
+
+        student.name = "Dasha";
+        student.age = 21;
+        student.major = "ICT2";
+        student.gpa = 4.7;
+        database.push_back(student);
+
+        student.name = "Max";
+        student.age = 21;
+        student.major = "ICT";
+        student.gpa = 5.0;
+        database.push_back(student);
+
+        // Act
+        std::sort(database.begin(),database.end(), compareByName);
+
+        // Assert
+        for (int i = 0; i < database.size() - 1; i++){
+            if (database[i].name > database[i + 1].name){
+                std::cout << "Invalid Test #1" << '\n';
+                return;
+            }
+        }
+        std::cout << "Valid Test #1" <<'\n';
+    }
+
+    void Test2(){
+        // Arrange
+        std::vector<Student> database;
+        Student student;
+
+        student.name = "Katya";
+        student.age = 20;
+        student.major = "ICT";
+        student.gpa = 4.9;
+        database.push_back(student);
+
+        student.name = "Dasha";
+        student.age = 21;
+        student.major = "ICT2";
+        student.gpa = 4.7;
+        database.push_back(student);
+
+        student.name = "Max";
+        student.age = 21;
+        student.major = "ICT";
+        student.gpa = 5.0;
+        database.push_back(student);
+
+        // Act
+        std::sort(database.begin(),database.end(), compareByGpa);
+
+        // Assert
+        for (int i = 0; i < database.size() - 1; i++){
+            if (database[i].gpa < database[i + 1].gpa){
+                std::cout << "Invalid Test #2" << '\n';
+                return;
+            }
+        }
+        std::cout << "Valid Test #2" << '\n';
+    }
+
+    void Test3(){
+        // Arrange
+        std::vector<Student> database;
+        Student student;
+
+        student.name = "Katya";
+        student.age = 20;
+        student.major = "ICT";
+        student.gpa = 4.9;
+        database.push_back(student);
+
+        student.name = "Dasha";
+        student.age = 21;
+        student.major = "ICT2";
+        student.gpa = 4.7;
+        database.push_back(student);
+
+        student.name = "Max";
+        student.age = 21;
+        student.major = "ICT";
+        student.gpa = 5.0;
+        database.push_back(student);
+
+        // Act
+        bool result = isStudentInDatabase(database, "Kirill");
+
+        // Assert
+        if (result){
+            std::cout << "Invalid Test #3" << '\n';
+        }
+        else{
+            std::cout << "Valid Test #3" << '\n';
+        }
+    }
+
+    void Test4(){
+        // Arrange
+        std::vector<Student> database;
+        Student student;
+
+        student.name = "Katya";
+        student.age = 20;
+        student.major = "ICT";
+        student.gpa = 4.9;
+        database.push_back(student);
+
+        student.name = "Dasha";
+        student.age = 21;
+        student.major = "ICT2";
+        student.gpa = 4.7;
+        database.push_back(student);
+
+        student.name = "Max";
+        student.age = 21;
+        student.major = "ICT";
+        student.gpa = 5.0;
+        database.push_back(student);
+
+        // Act
+        bool result = isStudentInDatabase(database, "Dasha");
+
+        // Assert
+        if (!result){
+            std::cout << "Invalid Test #4" << '\n';
+        }
+        else{
+            std::cout << "Valid Test #4" << '\n';
+        }
+    }
+
+    void Test5(){
+        // Arrange
+        Student student;
+        student.name = "Katya";
+        student.age = 20;
+        student.major = "ICT";
+        student.gpa = 4.9;
+
+        Student student_2;
+        student_2.name = "Dasha";
+        student_2.age = 21;
+        student_2.major = "ICT2";
+        student_2.gpa = 4.7;
+
+        // Act
+        bool result = compareByName(student,student_2);
+
+        //Assert
+        if (result){
+            std::cout << "Invalid Test #5" << '\n';
+        }
+        else{
+            std::cout << "Valid Test #5" << '\n';
+        }
+
+    }
+public:
+    void AllTest(){
+        Test1();
+        Test2();
+        Test3();
+        Test4();
+        Test5();
+    }
+};
 
 
 
@@ -80,13 +258,6 @@ int main() {
         switch (choice) {
             case 1:
                 addStudent(database);
-                break;
-                // После добавления студента, проверяем, есть ли он в базе данных
-                if (isStudentInDatabase(database, database.back().name)) {
-                    std::cout << "Студент уже существует в базе данных.\n";
-                } else {
-                    std::cout << "Студент добавлен в базу данных.\n";
-                }
                 break;
             case 2:
                 displayStudents(database);
