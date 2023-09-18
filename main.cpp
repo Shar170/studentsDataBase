ef23f059373a6e141478244905fab308e1aecce2
dariaakimova18 <100161946+dariaakimova18@users.noreply.github.com>
2023-09-19 02:32:02 +0300

    add test

--- main.cpp
+++ main.cpp
@@ -37,29 +37,206 @@ void displayStudents(const std::vector<Student>& database) {
 }
 
 // Функция для поиска студента по имени или по специальности
-void searchStudents(const std::vector<Student>& database) {
-    std::vector<Student> results;
-    std::string query;
-    std::cout << "Введите имя или специальность студента: ";
-    std::cin >> query;
-    for (const Student& student : database) {
-        if (student.name.find(query) != std::string::npos)
-            results.push_back(student);
-        else if (student.major.find(query) != std::string::npos) {
-            results.push_back(student);
+
+Student* findStudentByName(std::vector<Student*>& database, std::string name) {
+    for (Student* student : database) {
+        if (student->name == name) {
+            return student;
         }
     }
-    if (results.empty()) {
-        std::cout << "Нет совпадений." << std::endl;
-    }
-    else
-    {
-        std::cout << "Результаты поиска:" << std::endl;
-        for (const Student& student : results) {
-            std::cout << "Имя: " << student.name << ", Специальность: " << student.major << std::endl;
-        }
-    }
+    return nullptr;
 }
+
+Student* findStudentByMajor(std::vector<Student*>& database, std::string major) {
+    for (Student* student : database) {
+        if (student->major == major) {
+            return student;
+        }
+    }
+    return nullptr;
+}
+
+class Test {
+    void Test1() {
+        // Arrange
+        std::vector<Student*> database;
+        Student student1;
+
+        student1.name = "Даша";
+        student1.age = 21;
+        student1.major = "IT";
+        student1.gpa = 5;
+        database.push_back(&student1);
+
+        Student student2;
+        student2.name = "Саша";
+        student2.age = 18;
+        student2.major = "Менеджер";
+        student2.gpa = 3;
+        database.push_back(&student2);
+
+        Student student3;
+        student3.name = "Сергей";
+        student3.age = 20;
+        student3.major = "Сварщик";
+        student3.gpa = 4;
+        database.push_back(&student3);
+
+        // Act
+        Student *student_name = findStudentByName(database, "Даша");
+       
+        // Assert
+        if (student_name->name == "Даша") 
+            std::cout << "Тест 1 пройден\n";
+        else std::cout << "Тест 1 не пройден\n";
+    }
+
+    void Test2() {
+        // Arrange
+        std::vector<Student*> database;
+        Student student1;
+
+        student1.name = "Даша";
+        student1.age = 21;
+        student1.major = "IT";
+        student1.gpa = 5;
+        database.push_back(&student1);
+
+        Student student2;
+        student2.name = "Саша";
+        student2.age = 18;
+        student2.major = "Менеджер";
+        student2.gpa = 3;
+        database.push_back(&student2);
+
+        Student student3;
+        student3.name = "Сергей";
+        student3.age = 20;
+        student3.major = "Сварщик";
+        student3.gpa = 4;
+        database.push_back(&student3);
+
+        // Act
+        Student* student_major = findStudentByMajor(database, "Менеджер");
+
+        // Assert
+        if (student_major->major == "Менеджер")
+            std::cout << "Тест 2 пройден\n";
+        else std::cout << "Тест 2 не пройден\n";
+    }
+
+    void Test3() {
+        // Arrange
+        std::vector<Student*> database;
+        Student student1;
+
+        student1.name = "Даша";
+        student1.age = 21;
+        student1.major = "IT";
+        student1.gpa = 5;
+        database.push_back(&student1);
+
+        Student student2;
+        student2.name = "Саша";
+        student2.age = 18;
+        student2.major = "Менеджер";
+        student2.gpa = 3;
+        database.push_back(&student2);
+
+        Student student3;
+        student3.name = "Сергей";
+        student3.age = 20;
+        student3.major = "Сварщик";
+        student3.gpa = 4;
+        database.push_back(&student3);
+
+        // Act
+        Student* student_name = findStudentByName(database, "Света");
+
+        // Assert
+        if (student_name == nullptr)
+            std::cout << "Тест 3 пройден\n";
+        else std::cout << "Тест 3 не пройден\n";
+    }
+
+    void Test4() {
+        // Arrange
+        std::vector<Student*> database;
+        Student student1;
+
+        student1.name = "Даша";
+        student1.age = 21;
+        student1.major = "IT";
+        student1.gpa = 5;
+        database.push_back(&student1);
+
+        Student student2;
+        student2.name = "Саша";
+        student2.age = 18;
+        student2.major = "Менеджер";
+        student2.gpa = 3;
+        database.push_back(&student2);
+
+        Student student3;
+        student3.name = "Сергей";
+        student3.age = 20;
+        student3.major = "Сварщик";
+        student3.gpa = 4;
+        database.push_back(&student3);
+
+        // Act
+        Student* student_major = findStudentByMajor(database, "Врач");
+
+        // Assert
+        if (student_major == nullptr)
+            std::cout << "Тест 4 пройден\n";
+        else std::cout << "Тест 4 не пройден\n";
+    }
+
+    void Test5() {
+        // Arrange
+        std::vector<Student*> database;
+        Student student1;
+
+        student1.name = "Саша";
+        student1.age = 21;
+        student1.major = "IT";
+        student1.gpa = 5;
+        database.push_back(&student1);
+
+        Student student2;
+        student2.name = "Саша";
+        student2.age = 18;
+        student2.major = "Менеджер";
+        student2.gpa = 3;
+        database.push_back(&student2);
+
+        Student student3;
+        student3.name = "Сергей";
+        student3.age = 20;
+        student3.major = "Сварщик";
+        student3.gpa = 4;
+        database.push_back(&student3);
+
+        // Act
+        Student* student_name = findStudentByName(database, "Саша");
+        Student* student_major = findStudentByMajor(database, "Менеджер");
+
+        // Assert
+        if (student_major->major == "Менеджер" && student_name->name == "Саша")
+            std::cout << "Тест 5 пройден\n";
+        else std::cout << "Тест 5 не пройден\n";
+    }
+
+    public:
+        void allTests() {
+            Test1();
+            Test2();
+            Test3();
+            Test4();
+            Test5();
+        }
+};
 
 int main() {
     std::vector<Student> database;
