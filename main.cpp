898fbdd21e7b6863991c33eb38290e6d729604b3
dariaakimova18 <100161946+dariaakimova18@users.noreply.github.com>
2023-09-19 03:06:03 +0300

    added tests

--- main.cpp
+++ main.cpp
@@ -1,60 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "StudentDatabase.h"
 
-struct Student {
-    std::string name;
-    int age;
-    std::string major;
-    double gpa;
-};
-
-// Функция для добавления студента в базу данных
-void addStudent(std::vector<Student>& database) {
-    Student student;
-    std::cout << "Введите имя студента: ";
-    std::cin >> student.name;
-    std::cout << "Введите возраст студента: ";
-    std::cin >> student.age;
-    std::cout << "Введите специальность студента: ";
-    std::cin >> student.major;
-    std::cout << "Введите средний балл студента: ";
-    std::cin >> student.gpa;
-
-    database.push_back(student);
-    std::cout << "Студент добавлен в базу данных.\n";
-}
-
-// Функция для вывода всех студентов из базы данных
-void displayStudents(const std::vector<Student>& database) {
-    std::cout << "Список студентов:\n";
-    for (const Student& student : database) {
-        std::cout << "Имя: " << student.name << "\n";
-        std::cout << "Возраст: " << student.age << "\n";
-        std::cout << "Специальность: " << student.major << "\n";
-        std::cout << "Средний балл: " << student.gpa << "\n\n";
-    }
-}
-
-// Функция для поиска студента по имени или по специальности
-
-Student* findStudentByName(std::vector<Student*>& database, std::string name) {
-    for (Student* student : database) {
-        if (student->name == name) {
-            return student;
-        }
-    }
-    return nullptr;
-}
-
-Student* findStudentByMajor(std::vector<Student*>& database, std::string major) {
-    for (Student* student : database) {
-        if (student->major == major) {
-            return student;
-        }
-    }
-    return nullptr;
-}
 
 class Test {
     void Test1() {
@@ -238,8 +186,13 @@ class Test {
         }
 };
 
+
+
 int main() {
+    setlocale(LC_ALL, "ru");
     std::vector<Student> database;
+    Test* test = new Test();
+    test->allTests();
 
     int choice;
     do {
@@ -259,7 +212,7 @@ int main() {
             displayStudents(database);
             break;
         case 3:
-            searchStudents(database);
+            //searchStudents(database);
             break;
         case 0:
             std::cout << "Выход из программы.\n";
