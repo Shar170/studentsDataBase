6f8f4770d0fb994b66b6269a2ff090f5e6199702
ogkolya <99871609+ogkolya@users.noreply.github.com>
2023-09-20 01:19:41 +0300

    Update database.h

--- database.h
+++ database.h
@@ -66,38 +66,3 @@ void createReport(const std::vector<Student>& database) {
         std::cerr << "Error opening the file for writing.\n";
     }
 }
-// Commit 5
-// Updated the main function with a new menu option to create and save a report
-int main() {
-    std::vector<Student> database;
-
-    int choice;
-    do {
-        std::cout << "Menu:\n";
-        std::cout << "1. Add a student\n";
-        std::cout << "2. Display the list of students\n";
-        std::cout << "3. Create and save a report\n"; // New option
-        std::cout << "0. Exit\n";
-        std::cout << "Select an action: ";
-        std::cin >> choice;
-
-        switch (choice) {
-        case 1:
-            addStudent(database);
-            break;
-        case 2:
-            displayStudents(database);
-            break;
-        case 3:
-            createReport(database); // Call the function to create a report
-            break;
-        case 0:
-            std::cout << "Exiting the program.\n";
-            break;
-        default:
-            std::cout << "Invalid choice. Please try again.\n";
-        }
-    } while (choice != 0);
-
-    return 0;
-}
