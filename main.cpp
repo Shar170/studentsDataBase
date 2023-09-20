bf5a1d5f498501aa532fe2a15ff702cd2ecd3025
Anton Kurashkevich <mr.kur.anton@gmail.com>
2023-09-20 12:57:11 +0300

    Groups functionality moved to function "get_group_number". Added Google Tests

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "pch.h"
 
 using namespace std;
 
-vector<string> groups = {"КС-40", "КС-43", "КС-46"};
+vector<string> groups = { "КС-40", "КС-43", "КС-46" };
+int group_number = 0;
 
 struct Student {
     string name;
     int age;
     string major;
     double gpa;
-	string group;
+    string group;
 };
+
+bool get_group_number(int &group_number) {
+    cin >> group_number;
+    if (group_number < 1 || group_number > groups.size()) {
+        cout << "Введите правильный номер: ";
+        return false;
+    }
+    return true;
+}
 
 // Функция для добавления студента в базу данных
 void addStudent(vector<Student>& database) {
     Student student;
-	int group_number;
     cout << "Введите имя студента: ";
     cin >> student.name;
     cout << "Введите возраст студента: ";
@@ -26,15 +36,12 @@ void addStudent(vector<Student>& database) {
     cin >> student.major;
     cout << "Введите средний балл студента: ";
     cin >> student.gpa;
-	cout << "Выберите группу студента: " << "\n";
-	for (int i = 1; i <= groups.size(); i++)
-		cout << i << ". " << groups[i - 1] << "\n";
-	cin >> group_number; 
-	while (group_number < 1 || group_number > groups.size()) {
-		cout << "Введите правильный номер: ";
-		cin >> group_number; 
-	}
-	student.group = groups[group_number - 1];
+    cout << "Выберите группу студента: " << "\n";
+    for (int i = 1; i <= groups.size(); i++)
+        cout << i << ". " << groups[i - 1] << "\n";
+    while (!get_group_number(group_number))
+        continue;
+    student.group = groups[group_number - 1];
     database.push_back(student);
     cout << "Студент добавлен в базу данных.\n";
 }
@@ -47,7 +54,7 @@ void displayStudents(const vector<Student>& database) {
         cout << "Возраст: " << student.age << "\n";
         cout << "Специальность: " << student.major << "\n";
         cout << "Средний балл: " << student.gpa << "\n";
-		cout << "Группа: " << student.group << "\n\n";
+        cout << "Группа: " << student.group << "\n\n";
     }
 }
 
@@ -64,19 +71,38 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1:
-                addStudent(database);
-                break;
-            case 2:
-                displayStudents(database);
-                break;
-            case 0:
-                cout << "Выход из программы.\n";
-                break;
-            default:
-                cout << "Неверный выбор. Попробуйте снова.\n";
+        case 1:
+            addStudent(database);
+            break;
+        case 2:
+            displayStudents(database);
+            break;
+        case 0:
+            cout << "Выход из программы.\n";
+            break;
+        default:
+            cout << "Неверный выбор. Попробуйте снова.\n";
         }
     } while (choice != 0);
 
     return 0;
+}
+
+TEST(GroupTesting, HandlesBelowNormalInput) {
+    group_number = 0;
+    EXPECT_EQ(get_group_number(group_number), 0);
+}
+
+TEST(GroupTesting, HandlesNormalInput) {
+    group_number = 1;
+    EXPECT_EQ(get_group_number(group_number), 1);
+    group_number = 2;
+    EXPECT_EQ(get_group_number(group_number), 1);
+    group_number = 3;
+    EXPECT_EQ(get_group_number(group_number), 1);
+}
+
+TEST(GroupTesting, HandlesAboveNormalInput) {
+    group_number = 4;
+    EXPECT_EQ(get_group_number(group_number), 0);
 }
